0eaf96136789b35e29454691068ab4573e4be5fb
SYH05129 <140293555+SYH05129@users.noreply.github.com>
2023-09-01 08:44:52 +0000

    modify

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 import random
-def get_all_names():
+def __get_all_names():
     with open('names.txt',mode='r',encoding='utf-8') as file:
         names:list[str] = []
         for line in file:
@@ -7,20 +7,17 @@ def get_all_names():
             names.append(line)
         return names
 
-def get_random_names(nums):
-    names=get_all_names()
+def __get_random_names(nums):
+    names=__get_all_names()
     random_list=random.choices(names,k=nums)
     return random_list
 
 def get_students(student_num):
     students:list[list]=[]
-    names=get_random_names(nums=student_num)
+    names=__get_random_names(nums=student_num)
     for i in range(student_num):
         one_student=[random.randint(50,100)for _ in range(5)]
         name=names[i]
         one_student=[name]+one_student
         students.append(one_student)
     return students
-import student
-nums=int(input("請輸入學生的數量:"))
-print(student.get_students(student_num=nums))
